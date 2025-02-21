c458a64f124d9f52d59f32bf79cb6781de629e7e
Kamrul Hassan <kamrulthedev@gmail.com>
2025-02-21 12:24:57 +0600

    update-package-file

--- src/app/projectsArchive/page.jsx
+++ src/app/projectsArchive/page.jsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 import { GoArrowLeft } from "react-icons/go";
 const ProjectsArchive = () => {
   return (
-    <div className="container mx-auto md:px-[8%] px-4 pt-20">
+    <div className="container mx-auto md:px-[8%] px-4 pt-20 font-serif">
       <div>
         <Link
           href="/"
-          className="flex w-fit items-center gap-2 text-teal-400 group"
+          className="flex w-fit items-center gap-2 text-teal-400 group font-serif" 
         >
           <GoArrowLeft className="group-hover:-translate-x-1 transition-all text-xl" />{" "}
           Kamrul Hassan
         </Link>
-        <h1 className="text-5xl text-slate-100 font-semibold font-sans">
+        <h1 className="text-5xl text-slate-100 font-semibold">
           All Projects
         </h1>
       </div>
       <main>
-        <h2 className="text-3xl text-slate-100 font-semibold font-sans mt-10">
+        <h2 className="text-3xl text-slate-100 font-semibold mt-10">
           Project Archive coming sooooooon!
         </h2>
       </main>
